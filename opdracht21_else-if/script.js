c4c5d12e4684b21d31fc92506e434af6151717e3
vancapelledavid <71630143+vancapelledavid@users.noreply.github.com>
2020-11-12 12:56:59 +0100

    Add files via upload

---
+++ opdracht21_else-if/script.js
@@ -0,0 +1,31 @@
+const age= 19;
+const isfemale= true;
+const driverstatus= '';
+
+if(age>17){
+
+    console.log("Ok, deze is 18 of ouder, mag naar binnen");
+
+} else if(age<18){
+
+    console.log("Niet oud genoeg. Stuur weg.")
+}
+
+if(isfemale){
+
+    console.log("Is vrouw, mag naar binnen.")
+
+} else{
+
+    console.log ("Geen vrouw, stuur weg")
+}
+
+if(driverstatus == 'bob'){
+
+    console.log ("Is bob, mag rijden.")
+
+} else{
+    
+    console.log("Is geen bob. Mag niet rijden.")
+
+}
